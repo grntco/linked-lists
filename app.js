dd2fb4a8f57896e4325d6a756102096407fc3037
Grant Collins <grantcollins03@gmail.com>
2023-09-26 06:49:19 -0400

    fix append / prepend to not use arrays

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
 class LinkedList {
-    constructor(array = []) {
-        this.allNodes = array;
+    constructor() {
+        this.head = null;
     }
 
     append(value) {
-        const currentLastNode = this.allNodes.find(node => node.nextNode === null);
-        currentLastNode.nextNode = new Node(value);
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let currentNode = this.head;
+            while (currentNode.nextNode) {
+                currentNode = currentNode.nextNode;
+            }
+            currentNode.nextNode = newNode;
+        }
+    }
+
+    prepend(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let currentNode = this.head;
+            this.head = newNode;
+            newNode.nextNode = currentNode;
+        }
+    }
+
+    size() {
+        if (!this.head) {
+            return 0;
+        } else {
+
+        }
+    }
+
+    head() {
+        return this.allNodes[0];
+    }
+
+    tail() {
+        return this.allNodes.find(node => node.nextNode === null);
     }
 
 
@@ -18,9 +53,4 @@ class Node {
     }
 }
 
-const newNode = new Node();
-
-console.log(newNode);
-console.log(Node.allInstances)
-
-console.log(LinkedList.allNodes);
+LinkedList.append(value);
